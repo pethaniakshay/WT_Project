cea4c48fcd352bdf1c756297af5eb17e636f0a9f
pethaniakshay <patelaksh412@gmail.com>
2016-04-24 15:22:09 +0530

    Header complete with small image only

---
+++ header.php
@@ -0,0 +1,4 @@
+<div class="head">
+	<img  src="img/2.png" class="head_img" alt="Revolution">
+
+</div>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Revolution</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head> 
+		
+	<body>
+		<?php
+			include('header.php');
+		?>
+		
+	</body>
+
+
+</html>
